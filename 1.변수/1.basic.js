7a4cf0d0cc94a54f3e235737e0d2dcd0b7346a10
unknown <my@kitt.com>
2021-01-05 11:58:48 +0900

    변수식별자

---
+++ 1.변수/1.basic.js
@@ -0,0 +1,22 @@
+
+// 변수 선언
+var total;
+// log + tab :로그 콘솔 단축키
+console.log(total);
+
+// 변수초기화 : 변수에 시작값을 넣는다(reset 아니다.)
+total = 100;
+console.log(total);
+
+var age = 27;
+console.log(age);
+
+total = 50;
+console.log(total);
+
+var copyAge = age;
+console.log(copyAge);
+
+var x = 10;
+x = x + 3;
+console.log(x);
---
+++ 1.변수/2.identifier.js
@@ -0,0 +1,14 @@
+var apple ="애플";
+console.log(apple);
+
+var banana ="바나나";
+console.log(banana);
+// alt + shift :빠른 복사
+var BANANA = "바나나";
+
+
+var num79;
+console.log(num79);
+
+//사용자 이름을 저장할 거야
+//구체적으로 이름을 지어주세요.
