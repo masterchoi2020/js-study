641974f669ab4427b9ddc173d687555458ba244b
masterchoi2020 <75961230+masterchoi2020@users.noreply.github.com>
2021-01-05 17:17:40 +0900

    Update 2.identifier.js

--- 1.변수/2.identifier.js
+++ 1.변수/2.identifier.js
@@ -1,14 +1,36 @@
-var apple ="애플";
+var apple = "사과";
+var apple = "애플";
 console.log(apple);
 
-var banana ="바나나";
+
+
+
+var banana = "바나나";
+// alt + shift : 빠른 복사
 console.log(banana);
-// alt + shift :빠른 복사
-var BANANA = "바나나";
 
+var BANANA = "빠나나";
+console.log(BANANA);
 
-var num79;
+// var my-name;
+var my_name;    //(O)
+var $myname;    //(O)
+
+// var 79num;   //(x)
+var num79;      //(O)
+var num79ber;   //(0)
 console.log(num79);
 
+잘못된 변수명
+// var var;     //(x)
+// var if;      //(x)
+// var my phone number  //(x)
+
+var myhponenumberforourcompany;
+var myPhoneNumberForOurCompany;
+
 //사용자 이름을 저장할 거야
-//구체적으로 이름을 지어주세요.
+//구체적으로 이름을 지어 주세요
+
+// var x; (x)
+var userName;
