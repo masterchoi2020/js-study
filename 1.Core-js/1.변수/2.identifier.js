01db02623a6dde5dff1b33d9392287a56794bf86
masterchoi2020 <75961230+masterchoi2020@users.noreply.github.com>
2022-12-28 03:48:17 +0900

    Update 2.identifier.js

--- 1.Core-js/1.변수/2.identifier.js
+++ 1.Core-js/1.변수/2.identifier.js
@@ -1,34 +1,40 @@
-var apple = "사과";
+// var 사용시 문제점: var apple = "사과"; var apple = "애플" 처럼 
+// 변수 앞에 var를 계속사용할 수 있어 변수 선언의 의미가 없음
+
+// var apple = "사과";
 var apple = "애플";
 console.log(apple);
 
 
-var banana = "바나나";
+let banana = "바나나";
 // alt + shift : 빠른 복사
 console.log(banana);
 
-var BANANA = "빠나나";
+let BANANA = "빠나나";
 console.log(BANANA);
 
-// var my-name;
-var my_name;    //(O)
-var $myname;    //(O)
+// var my-name; //(x)
+let my_name;    //(O)
+console.log(my_name);
+let $myname;    //(O)
+console.log($myname);
+
 
 // var 79num;   //(x)
-var num79;      //(O)
-var num79ber;   //(0)
+let num79;      //(O)
+let num79ber;   //(0)
 console.log(num79);
 
 //잘못된 변수명
-// var var;     //(x)
-// var if;      //(x)
-// var my phone number  //(x)
+// let var;     //(x)
+// let if;      //(x)
+// let my phone number  //(x)
 
-var myhponenumberforourcompany;
-var myPhoneNumberForOurCompany;
+let myhponenumberforourcompany;
+let myPhoneNumberForOurCompany;
 
-//사용자 이름을 저장할 거야
+//사용자 이름을 저장
 //구체적으로 이름을 지어 주세요
 
-// var x; (x)
-var userName;
+// let x; (x)
+let userName;
